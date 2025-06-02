9043950e1e43025c6d22e987beb2d0ab895a7c7e
Ivan-Caringal <ivancaringal20@gmail.com>
2025-06-02 23:01:03 +0800

    feat:project start

---
+++ app/Models/Attendee.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;   // import User model
+use App\Models\Event;  // import Event model
+
+class Attendee extends Model
+{
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+}
