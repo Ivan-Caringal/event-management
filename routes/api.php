94ba7002039f427d879a5219d14c259babcf7fc6
Ivan-Caringal <ivancaringal20@gmail.com>
2025-06-12 23:28:49 +0800

    feat: add sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,4 +13,8 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
     ->scoped()->except(['update']);
-    
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
